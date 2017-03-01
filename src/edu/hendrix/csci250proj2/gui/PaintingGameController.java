7e607e6694a64f165394295723d8f4e1739d72a4
Jacob Turner <me@jacobturner.me>
2017-03-01 11:19:43 -0600

    Revert "Eraser and Paint Tweaks"
    This reverts commit 69c98b5b9f25d828f11233b2d1ce9b34c631f6bc.

--- src/edu/hendrix/csci250proj2/gui/PaintingGameController.java
+++ src/edu/hendrix/csci250proj2/gui/PaintingGameController.java
@@ -17,8 +17,6 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -32,7 +30,6 @@ import javafx.scene.shape.StrokeLineCap;
 import javafx.scene.control.ProgressBar;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 
 public class PaintingGameController {
 	@FXML
@@ -51,16 +48,14 @@ public class PaintingGameController {
 	HBox drawingStuff;
 	@FXML
 	VBox colorStuff;
-	@FXML
-	ToggleButton eraser;
 	
 	private User user;
 	private int rating;
 	private int otherRating;
 	private double sx;
 	private double sy;
-	static Color currentColor = Color.BLACK;
-	private double inkRemainingDubs = 1.0;
+	private Color currentColor = Color.BLACK;
+	private double inkRemainingDubs;
 	
 	@FXML
 	private void initialize() {
@@ -74,8 +69,8 @@ public class PaintingGameController {
 		drawingArea.setOnMouseDragged(event -> draw(event));
 		drawingArea.setOnMousePressed(event -> startDrag(event));
 		drawingArea.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-//		drawingArea.setLayoutX(colorStuff.getWidth());
-//		drawingArea.setLayoutY(drawingStuff.getHeight());
+		drawingArea.setLayoutX(colorStuff.getWidth());
+		drawingArea.setLayoutY(drawingStuff.getHeight());
 		TextInputDialog signInDialog = new TextInputDialog();
 		signInDialog.setTitle("Painting Game");
 		signInDialog.setHeaderText("Welcome to Painting Game");
@@ -90,7 +85,6 @@ public class PaintingGameController {
 		} catch (Exception exc) {
 			outputMessage(exc.getMessage(), AlertType.ERROR);
 		}
-		
 	}
 	
 	public void startDrag(MouseEvent event) {
@@ -102,39 +96,13 @@ public class PaintingGameController {
 		double fx = event.getX();
 		double fy = event.getY();
 		Line line = new Line(sx, sy, fx, fy);
-		if (eraser.isSelected()) {
-			if (fx < 50 || fx > drawingArea.getWidth()-50 || 
-				fy < 0+50 || fy > drawingArea.getHeight()-50) {
-				
-			}
-		}
-		if (fx < 0 || fx > drawingArea.getWidth() || 
-				fy < 0 || fy > drawingArea.getHeight()) {
-			line.setStroke(null);		
-		} else {
-			line.setStroke(currentColor);
-			line.setStrokeLineCap(StrokeLineCap.ROUND);
-			if (eraser.isSelected()) {
-				line.setStrokeWidth(20.0); 
-			}
-			drawingArea.getChildren().add(line);
-			sx = fx;
-			sy = fy;
-			if (!eraser.isSelected()) {
-				inkRemainingDubs -= .001;
-				inkRemaining.setProgress(inkRemainingDubs);
-			}
-		}
-	}
-	
-	@FXML
-	private void erase() {
-		if (eraser.isSelected()) {
-			currentColor = Color.WHITE;
-		} else {
-			currentColor = colorChooser.getValue();
-		}
-		
+		line.setStroke(currentColor);
+		line.setStrokeLineCap(StrokeLineCap.ROUND);
+		drawingArea.getChildren().add(line);
+		sx = fx;
+		sy = fy;
+		inkRemainingDubs -= .001;
+		inkRemaining.setProgress(inkRemainingDubs);
 	}
 	
 	private void setDone() {
