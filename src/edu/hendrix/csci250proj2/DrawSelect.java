137c5181037cf175a5748e5953519a70940dca1f
Jacob Turner <me@jacobturner.me>
2017-03-08 01:07:45 -0600

    combined files into DrawSelect

--- src/edu/hendrix/csci250proj2/DrawSelect.java
+++ src/edu/hendrix/csci250proj2/DrawSelect.java
@@ -1,14 +1,26 @@
 package edu.hendrix.csci250proj2;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class DrawSelect {
 	public static String initialize() throws FileNotFoundException {
 		String chosenPrompt = "";
-		ArrayList<String> potentialDrawings = DrawA.readFile();
+		ArrayList<String> potentialDrawings = readFile();
 		chosenPrompt = potentialDrawings.get(ThreadLocalRandom.current().nextInt(potentialDrawings.size()));
 		return chosenPrompt;
+	}
+	
+	public static ArrayList<String> readFile() throws FileNotFoundException {
+		ArrayList<String> potentialDrawings = new ArrayList<String>();
+		Scanner fileScanner = new Scanner(new File("./resources/DrawAWhat.txt"));
+		while (fileScanner.hasNextLine()) {
+			potentialDrawings.add(fileScanner.nextLine());
+		}
+		fileScanner.close();
+		return potentialDrawings;
 	}
 }
